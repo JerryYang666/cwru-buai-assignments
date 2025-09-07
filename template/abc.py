928a570efdeb45026716d5ff2eebbc2c0949f28c
Ruihuang Yang <rxy216@case.edu>
2025-09-07 16:00:07 -0700

    Add import statements for pandas, numpy, matplotlib, and seaborn in template; update pyproject.toml to include new dependencies for matplotlib and seaborn.

--- template/abc.py
+++ template/abc.py
@@ -4,6 +4,15 @@
 # NetID: [Your NetID]  
 # Date: [Your Date]  
 
+# %% [markdown]
+# ### Import libraries
+
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # %%
 def f(x):
   return 3*x+1
