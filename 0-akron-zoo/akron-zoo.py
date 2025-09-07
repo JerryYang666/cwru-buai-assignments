14f4cc15574bde7b822843054996c3dda75f036a
Ruihuang Yang <rxy216@case.edu>
2025-09-07 15:59:39 -0700

    Add import statements for pandas, numpy, matplotlib, and seaborn in Akron Zoo notebook and script; update dependencies in pyproject.toml and uv.lock to include matplotlib and seaborn.

--- 0-akron-zoo/akron-zoo.py
+++ 0-akron-zoo/akron-zoo.py
@@ -4,6 +4,15 @@
 # NetID: rxy216  
 # Date: 09/07/2025  
 
+# %% [markdown]
+# ### Import libraries
+
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # %%
 def f(x):
   return 3*x+1
